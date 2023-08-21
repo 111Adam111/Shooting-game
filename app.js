694c14ece36ecb243f7d1ee29de42a78548f7799
Adam Dunowski <70273104+111Adam111@users.noreply.github.com>
2023-08-21 16:39:00 +0200

    typescript added

--- app.js
+++ app.js
@@ -1,162 +1,145 @@
 // HTML elements
-const gameWindow = document.getElementsByClassName('game-window')[0]
-const startScreen = document.getElementsByClassName('start-screen')[0]
-const endScreen = document.getElementsByClassName('end-screen')[0]
-const menu = document.getElementsByClassName('menu')[0]
-
-
+var gameWindowEl = document.getElementsByClassName('game-window')[0];
+var startScreenEl = document.getElementsByClassName('start-screen')[0];
+var endScreenEL = document.getElementsByClassName('end-screen')[0];
+var menuEl = document.getElementsByClassName('menu')[0];
+var timerEl = document.getElementById('timer');
+var scoreEl = document.getElementById('score');
+var statsSCoreEL = document.getElementById('stats-score');
+var targetsShotEL = document.getElementById('targets-shot');
+var shotsPerSecondEL = document.getElementById('shots-per-second');
+var avgScorePerTargetEl = document.getElementById('avg-score-per-target');
 // game settings
-let targetAmount = 5;
-let totalScore = 0;
-const gameDuration = 30;
-let countDown = gameDuration;
-let shots = 0;
-let isRunning = false;
-let canRestart = false;
-const maxPointsPerShot = 100;
-
-
+var targetAmount = 5;
+var totalScore = 0;
+var gameDuration = 30;
+var countDown = gameDuration;
+var shots = 0;
+var isRunning = false;
+var canRestart = false;
+var maxPointsPerShot = 100;
 // Click to start
-startScreen.addEventListener('click', () => {
-    start()
-    startScreen.classList.add('hide')   
-})
-
-
+startScreenEl.addEventListener('click', function () {
+    start();
+    startScreenEl.classList.add('hide');
+});
 // starts game and spawns entry targets
 function start() {
     if (!isRunning) {
         isRunning = true;
-        for (let i = 0; i < targetAmount; i++){
-            spawnTarget()
+        for (var i = 0; i < targetAmount; i++) {
+            spawnTarget();
         }
         countDown = gameDuration;
     }
 }
-
-
 // Counts time left and ends the game
-const gameEnder = setInterval(() => {
+var gameEnder = setInterval(function () {
     if (isRunning && countDown > 0) {
         countDown -= 0.1; // 100 milliseconds or 0.1 second
-        document.getElementById('timer').innerHTML 
-            = `Time: ${Math.abs(Math.round(countDown * 10) / 10).toFixed(1)}`;    
+        timerEl.innerHTML
+            = "Time: ".concat(Math.abs(Math.round(countDown * 10) / 10).toFixed(1));
     }
     if (countDown <= 0) {
-        isRunning = false
-        gameWindow.innerHTML = ''
-        endScreen.classList.remove('hide')
-        totalScore = 0
-        shots = 0
-        canRestartTimer()
+        isRunning = false;
+        gameWindowEl.innerHTML = '';
+        endScreenEL.classList.remove('hide');
+        totalScore = 0;
+        shots = 0;
+        canRestartTimer();
     }
 }, 100); // 100 milliseconds or 0.1 second
-
-
 // spawns target at random location within game window
 function spawnTarget() {
-    const element = document.createElement('div')
-    element.classList.add('target')
-    element.innerHTML = `<img src="target.png" alt="">`
-    const randomPosition = (max) => Math.random() * max
-    const windowWidth = gameWindow.clientWidth
-    const windowHeight = gameWindow.clientHeight
-    const elementSize = windowHeight / 5 // 19vh
-    const navbarHeight = windowHeight / 19 // 5vh
-    const targetX = randomPosition(windowWidth - elementSize)
-    const targetY = randomPosition(windowHeight - elementSize) + navbarHeight
-    element.style.left = `${targetX}px`
-    element.style.top = `${targetY}px`
-    element.addEventListener('mousedown', (click) => {
-        shot(click, element, targetX, targetY, elementSize)   
-    })   
-    gameWindow.appendChild(element) 
+    var element = document.createElement('div');
+    element.classList.add('target');
+    element.innerHTML = "<img src=\"public/target.png\" alt=\"\">";
+    var randomPosition = function (max) { return Math.random() * max; };
+    var windowWidth = gameWindowEl.clientWidth;
+    var windowHeight = gameWindowEl.clientHeight;
+    var elementSize = windowHeight / 5; // 19vh
+    var navbarHeight = windowHeight / 19; // 5vh
+    var targetX = randomPosition(windowWidth - elementSize);
+    var targetY = randomPosition(windowHeight - elementSize) + navbarHeight;
+    element.style.left = "".concat(targetX, "px");
+    element.style.top = "".concat(targetY, "px");
+    element.addEventListener('mousedown', function (click) {
+        shot({ click: click, element: element, targetX: targetX, targetY: targetY, elementSize: elementSize });
+    });
+    gameWindowEl.appendChild(element);
 }
-
-
 //  target click and score per shot calculation
-function shot(click, element, targetX, targetY, elementSize) {
+function shot(_a) {
+    var click = _a.click, element = _a.element, targetX = _a.targetX, targetY = _a.targetY, elementSize = _a.elementSize;
     // calculation
-    const radius = elementSize / 2
-    const targetCenterX = targetX + radius;
-    const targetCenterY = targetY + radius;
-    const distance = Math.sqrt((click.x - targetCenterX)**2 
-                            + (click.y - targetCenterY)**2);
-    let score = maxPointsPerShot - Math.floor(distance * maxPointsPerShot / radius)
+    var radius = elementSize / 2;
+    var targetCenterX = targetX + radius;
+    var targetCenterY = targetY + radius;
+    var distance = Math.sqrt(Math.pow((click.x - targetCenterX), 2)
+        + Math.pow((click.y - targetCenterY), 2));
+    var score = maxPointsPerShot - Math.floor(distance * maxPointsPerShot / radius);
     // action - updates score, removes target, spawns new target
     // and shows score for shot
     if (score > 0) {
-        scoreUpdate(score)
-        gameWindow.removeChild(element)
-        isRunning && spawnTarget()
-        hitValuePop(score, click)
-
+        scoreUpdate(score);
+        gameWindowEl.removeChild(element);
+        isRunning && spawnTarget();
+        hitValuePop(score, click);
     }
 }
-
-
 // pop up containing score for every shot
-let hitValuePop = (value, click) => {
-    const element = document.createElement('p')
-    element.classList.add('slide-out-top')
-    const pointsThreshold = 90;
-    let msg = value  
+var hitValuePop = function (value, click) {
+    var element = document.createElement('p');
+    element.classList.add('slide-out-top');
+    var pointsThreshold = 90;
+    var msg = value;
     if (value >= pointsThreshold && value !== maxPointsPerShot) {
-        msg = value + '!'
+        msg = value + '!';
     }
     else if (value == maxPointsPerShot) {
-        msg = 'Perfect Shot!'
+        msg = 'Perfect Shot!';
     }
-    element.innerHTML = `${msg}`
-    element.style.left = `${value == maxPointsPerShot ? click.x - 50 : click.x -10}px`
-    element.style.top = `${click.y -40}px`
-    gameWindow.appendChild(element)
-
+    element.innerHTML = "".concat(msg);
+    element.style.left = "".concat(value == maxPointsPerShot ? click.x - 50 : click.x - 10, "px");
+    element.style.top = "".concat(click.y - 40, "px");
+    gameWindowEl.appendChild(element);
     // pop up dispaears after 0.4 secounds
-    const popupTime = 400;
-    setTimeout(() => {
-        gameWindow.hasChildNodes() && gameWindow.removeChild(element)
-    }, popupTime)
-}
-
-
+    var popupTime = 400;
+    setTimeout(function () {
+        gameWindowEl.hasChildNodes() && gameWindowEl.removeChild(element);
+    }, popupTime);
+};
 // real time score update
-let scoreUpdate = (score) => {
-    totalScore += score
-    document.getElementById('score').innerHTML = `Score: ${totalScore}`
-    stats(totalScore)
-}   
-
-
+var scoreUpdate = function (score) {
+    totalScore += score;
+    scoreEl.innerHTML = "Score: ".concat(totalScore);
+    stats(totalScore);
+};
 // end screen stats
-let stats = (totalScore) => {
-    shots += 1
-    let shotsPS = shots / gameDuration
-    let avg = totalScore / shots
-    document.getElementById('stats-score').innerHTML = `Score: ${totalScore}`
-    document.getElementById('targets-shot').innerHTML = `Targets shot: ${shots}`
-    document.getElementById('shots-per-second').innerHTML = 
-        `Shots per second ${shotsPS.toFixed(2)}`
-    document.getElementById('avg-score-per-target').innerHTML = 
-        `Avg score per target: ${avg.toFixed(2)}`
-}  
-
-
+var stats = function (totalScore) {
+    shots += 1;
+    var shotsPS = shots / gameDuration;
+    var avg = totalScore / shots;
+    statsSCoreEL.innerHTML = "Score: ".concat(totalScore);
+    targetsShotEL.innerHTML = "Targets shot: ".concat(shots);
+    shotsPerSecondEL.innerHTML =
+        "Shots per second ".concat(shotsPS.toFixed(2));
+    avgScorePerTargetEl.innerHTML =
+        "Avg score per target: ".concat(avg.toFixed(2));
+};
 // Click to restart
-endScreen.addEventListener('click', () => {
+endScreenEL.addEventListener('click', function () {
     if (canRestart) {
-        start()
-        document.getElementById('score').innerHTML = 'Score'
-        endScreen.classList.add('hide')
-        canRestartTimer()      
+        start();
+        scoreEl.innerHTML = 'Score';
+        endScreenEL.classList.add('hide');
+        canRestartTimer();
     }
-})
-
-
+});
 // 1 sec restart delayer to prevent unwanted early restart
 function canRestartTimer() {
-    const resetTime = 1000; // milliseconds
-    setTimeout(() => {
+    var resetTime = 1000; // milliseconds
+    setTimeout(function () {
         canRestart = !canRestart;
-      }, resetTime);
+    }, resetTime);
 }
---
+++ app.ts
@@ -0,0 +1,180 @@
+// HTML elements
+const gameWindowEl = <HTMLElement>document.getElementsByClassName('game-window')[0]
+const startScreenEl = <HTMLElement>document.getElementsByClassName('start-screen')[0]
+const endScreenEL = <HTMLElement>document.getElementsByClassName('end-screen')[0]
+const menuEl = <HTMLElement>document.getElementsByClassName('menu')[0]
+const timerEl = <HTMLElement>document.getElementById('timer')
+const scoreEl = <HTMLElement>document.getElementById('score')
+const statsSCoreEL = <HTMLElement>document.getElementById('stats-score')
+const targetsShotEL = <HTMLElement>document.getElementById('targets-shot')
+const shotsPerSecondEL = <HTMLElement>document.getElementById('shots-per-second')
+const avgScorePerTargetEl = <HTMLElement>document.getElementById('avg-score-per-target')
+
+
+// game settings
+let targetAmount: number = 5;
+let totalScore: number = 0;
+const gameDuration: number = 30;
+let countDown: number = gameDuration;
+let shots: number = 0;
+let isRunning: boolean = false;
+let canRestart: boolean = false;
+const maxPointsPerShot: number = 100;
+
+
+
+// Click to start
+startScreenEl.addEventListener('click', () => {
+    start()
+    startScreenEl.classList.add('hide')   
+})
+
+
+// starts game and spawns entry targets
+function start(): void {
+    if (!isRunning) {
+        isRunning = true;
+        for (let i = 0; i < targetAmount; i++){
+            spawnTarget()
+        }
+        countDown = gameDuration;
+    }
+}
+
+
+// Counts time left and ends the game
+const gameEnder = setInterval((): void => {
+    if (isRunning && countDown > 0) {
+        countDown -= 0.1; // 100 milliseconds
+        timerEl.innerHTML 
+            = `Time: ${Math.abs(Math.round(countDown * 10) / 10).toFixed(1)}`;    
+    }
+    if (countDown <= 0) {
+        isRunning = false
+        gameWindowEl.innerHTML = ''
+        endScreenEL.classList.remove('hide')
+        totalScore = 0
+        shots = 0
+        canRestartTimer()
+    }
+}, 100); // 100 milliseconds
+
+
+// spawns target at random location within game window
+function spawnTarget(): void {
+    const element: HTMLElement = document.createElement('div')
+    element.classList.add('target')
+    element.innerHTML = `<img src="public/target.png" alt="">`
+    const randomPosition = (max: number): number => Math.random() * max
+    const windowWidth = gameWindowEl.clientWidth
+    const windowHeight = gameWindowEl.clientHeight
+    const elementSize = windowHeight / 5 // 19vh
+    const navbarHeight = windowHeight / 19 // 5vh
+    const targetX = randomPosition(windowWidth - elementSize)
+    const targetY = randomPosition(windowHeight - elementSize) + navbarHeight
+    element.style.left = `${targetX}px`
+    element.style.top = `${targetY}px`
+    element.addEventListener('mousedown', (click: MouseEvent) => {
+        shot({click, element, targetX, targetY, elementSize})
+    })   
+    gameWindowEl.appendChild(element) 
+}
+
+interface Shot {
+    click: MouseEvent,
+    element: HTMLElement,
+    targetX: number,
+    targetY: number,
+    elementSize: number
+}
+
+//  target click and score per shot calculation
+function shot({click, element, targetX, targetY, elementSize}: Shot): void {
+    // calculation
+    const radius = elementSize / 2
+    const targetCenterX = targetX + radius;
+    const targetCenterY = targetY + radius;
+    const distance = Math.sqrt((click.x - targetCenterX)**2 
+                            + (click.y - targetCenterY)**2);
+    let score: number = maxPointsPerShot - Math.floor(distance * maxPointsPerShot / radius)
+    // action - updates score, removes target, spawns new target
+    // and shows score for shot
+    if (score > 0) {
+        scoreUpdate(score)
+        gameWindowEl.removeChild(element)
+        isRunning && spawnTarget()
+        hitValuePop(score, click)
+
+    }
+}
+
+
+// pop up containing score for every shot
+let hitValuePop = (value: number, click: MouseEvent): void => {
+    const element: HTMLElement = document.createElement('p')
+    element.classList.add('slide-out-top')
+    const pointsThreshold: number = 90;
+    let msg: string | number = value  
+    if (value >= pointsThreshold && value !== maxPointsPerShot) {
+        msg = value + '!'
+    }
+    else if (value == maxPointsPerShot) {
+        msg = 'Perfect Shot!'
+    }
+    element.innerHTML = `${msg}`
+    element.style.left = `${value == maxPointsPerShot ? click.x - 50 : click.x -10}px`
+    element.style.top = `${click.y -40}px`
+    gameWindowEl.appendChild(element)
+
+    // pop up dispaears after 0.4 secounds
+    const popupTime = 400;
+    setTimeout(() => {
+        gameWindowEl.hasChildNodes() && gameWindowEl.removeChild(element)
+    }, popupTime)
+}
+
+
+// real time score update
+let scoreUpdate = (score: number): void => {
+    totalScore += score
+        scoreEl.innerHTML = `Score: ${totalScore}`
+    
+    stats(totalScore)
+}   
+
+
+
+
+
+// end screen stats
+let stats = (totalScore: number): void => {
+    shots += 1
+    let shotsPS: number = shots / gameDuration
+    let avg: number = totalScore / shots
+    statsSCoreEL.innerHTML = `Score: ${totalScore}`
+    targetsShotEL.innerHTML = `Targets shot: ${shots}`
+    shotsPerSecondEL.innerHTML = 
+        `Shots per second ${shotsPS.toFixed(2)}`
+        avgScorePerTargetEl.innerHTML = 
+        `Avg score per target: ${avg.toFixed(2)}`
+}  
+
+
+// Click to restart
+endScreenEL.addEventListener('click', () => {
+    if (canRestart) {
+        start()
+        scoreEl.innerHTML = 'Score'
+        endScreenEL.classList.add('hide')
+        canRestartTimer()      
+    }
+})
+
+
+// 1 sec restart delayer to prevent unwanted early restart
+function canRestartTimer(): void {
+    const resetTime: number = 1000; // milliseconds
+    setTimeout(() => {
+        canRestart = !canRestart;
+      }, resetTime);
+}
